60d5d65d23cebfe1613f52ae8a8936e413dba4bb
Esther Maina <mainaesther77@gmial.com>
2022-10-23 08:01:40 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
 # Your code here!
+#greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end 
+
+greet_programmer
+
+#greet(Naureen)
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet"Naureen"
+greet"Jimmy"
+
+#hello, sunny
+def greetWithDefault(name = "programmer")
+    puts "Hello, #{name}!"
+end 
+
+greetWithDefault"Naureen"
+greetWithDefault
+
+#sum add(1,2)
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum = add(1,2)
+
+#halve
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    return number / 2
+end
+
